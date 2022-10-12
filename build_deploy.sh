713f83f3cc156a4fb51dcc64efe00666cce8975d
zachacious <back4moore@gmail.com>
2022-10-12 17:55:11 -0500

    Updated builds, create tags, bump version to 1.0.10

--- build_deploy.sh
+++ build_deploy.sh
@@ -45,6 +45,15 @@ checksumNew="integrity=\"sha384-$checksum\""
 sed -i.bak -E "s/$sriRegex/$checksumNew/" ./README.md
 sed -i.bak -E "s/$sriRegex/$checksumNew/" ./browser-lib/demo/index.html
 
+# create a copy of browser lib build and add version number - save in versions folder
+cp ./browser-lib/dist/magnoliafabrics.min.js ./browser-lib/versions/magnoliafabrics-$version.min.js
+
+# replace brower-lib filename in README and in demo/index.html
+cdnVersionRegex="magnoliafabrics-(.*).min.js"
+cdnVersionNew="magnoliafabrics-$version.min.js",
+sed -i.bak -E "s/$cdnVersionRegex/$cdnVersionNew/" ./README.md
+sed -i.bak -E "s/$cdnVersionRegex/$cdnVersionNew/" ./browser-lib/demo/index.html
+
 # build the npm lib
 cd npm-lib
 yarn build
@@ -56,6 +65,204 @@ else
     echo "Npm-lib build failure"
     exit
 fi
+
+# publish to github registry
+cd npm-lib
+npm publish
+cd ..
+
+if [ $? -eq 0 ]; then
+    echo "NPM lib published successfully"
+else
+    echo "NPM lib publish failed"
+    exit
+fi
+
+################################
+# publish demo site
+
+# temporarily copy contents browser-lib/versions into demo folder
+cp ./browser-lib/versions/* ./demo
+
+BUCKET_NAME=magnolia-api-demo
+SRC=browser-lib/demo/
+CLOUDFRONT_DISTRIBUTION=ET9D78QSKOJQ6
+
+# copy all files to S3 bucket; delete any files in the bucket
+# that no longer exist locally in ${SRC} folder
+aws s3 sync ${SRC} s3://${BUCKET_NAME}/ --delete
+
+# fix html files
+aws s3 cp \
+      --exclude "*" \
+      --include "*.html" \
+      --content-type="text/html; charset=utf-8"  \
+      --cache-control="public, max-age=60" \
+      --metadata-directive="REPLACE" \
+      --recursive \
+       s3://${BUCKET_NAME}/ \
+       s3://${BUCKET_NAME}/
+
+# fix css files
+aws s3 cp \
+      --exclude "*" \
+      --include "*.css" \
+      --content-type="text/css; charset=utf-8"  \
+      --cache-control="public, max-age=31536000, immutable" \
+      --metadata-directive="REPLACE" \
+      --recursive \
+       s3://${BUCKET_NAME}/ \
+       s3://${BUCKET_NAME}/
+
+# fix js files
+aws s3 cp \
+      --exclude "*" \
+      --include "*.js" \
+      --content-type="text/javascript; charset=utf-8"  \
+      --cache-control="public, max-age=31536000, immutable" \
+      --metadata-directive="REPLACE" \
+      --recursive \
+       s3://${BUCKET_NAME}/ \
+       s3://${BUCKET_NAME}/
+
+# fix svg files
+aws s3 cp \
+      --exclude "*" \
+      --include "*.svg" \
+      --content-type="image/svg+xml"  \
+      --cache-control="public, max-age=31536000, immutable" \
+      --metadata-directive="REPLACE" \
+      --recursive \
+       s3://${BUCKET_NAME}/ \
+       s3://${BUCKET_NAME}/
+
+# fix png files
+aws s3 cp \
+      --exclude "*" \
+      --include "*.png" \
+      --content-type="image/png"  \
+      --cache-control="public, max-age=31536000, immutable" \
+      --metadata-directive="REPLACE" \
+      --recursive \
+       s3://${BUCKET_NAME}/ \
+       s3://${BUCKET_NAME}/
+
+# fix jpg/jpeg files
+aws s3 cp \
+      --exclude "*" \
+      --include "*.jpg" --include "*.jpeg" \
+      --content-type="image/jpeg"  \
+      --cache-control="public, max-age=31536000, immutable" \
+      --metadata-directive="REPLACE" \
+      --recursive \
+       s3://${BUCKET_NAME}/ \
+       s3://${BUCKET_NAME}/
+
+# fix xml files
+aws s3 cp \
+      --exclude "*" \
+      --include "*.xml" \
+      --content-type="application/xml; charset=utf-8"  \
+      --cache-control="public, max-age=60" \
+      --metadata-directive="REPLACE" \
+      --recursive \
+       s3://${BUCKET_NAME}/ \
+       s3://${BUCKET_NAME}/
+
+# fix xsl files
+aws s3 cp \
+      --exclude "*" \
+      --include "*.xsl" \
+      --content-type="application/xslt+xml; charset=utf-8"  \
+      --cache-control="public, max-age=60" \
+      --metadata-directive="REPLACE" \
+      --recursive \
+       s3://${BUCKET_NAME}/ \
+       s3://${BUCKET_NAME}/
+
+# fix txt files
+aws s3 cp \
+      --exclude "*" \
+      --include "*.txt" \
+      --content-type="text/plain; charset=utf-8"  \
+      --cache-control="public, max-age=60" \
+      --metadata-directive="REPLACE" \
+      --recursive \
+       s3://${BUCKET_NAME}/ \
+       s3://${BUCKET_NAME}/
+
+# fix json/map/topojson files
+aws s3 cp \
+      --exclude "*" \
+      --include "*.json" --include "*.map" --include "*.topojson" \
+      --content-type="application/json; charset=utf-8"  \
+      --cache-control="public, max-age=60" \
+      --metadata-directive="REPLACE" \
+      --recursive \
+       s3://${BUCKET_NAME}/ \
+       s3://${BUCKET_NAME}/
+
+# fix ico files
+aws s3 cp \
+      --exclude "*" \
+      --include "*.ico" \
+      --content-type="image/x-icon"  \
+      --cache-control="public, max-age=31536000, immutable" \
+      --metadata-directive="REPLACE" \
+      --recursive \
+       s3://${BUCKET_NAME}/ \
+       s3://${BUCKET_NAME}/
+
+# fix zip files
+aws s3 cp \
+      --exclude "*" \
+      --include "*.zip" \
+      --content-type="application/zip"  \
+      --cache-control="public, max-age=31536000, immutable" \
+      --metadata-directive="REPLACE" \
+      --recursive \
+       s3://${BUCKET_NAME}/ \
+       s3://${BUCKET_NAME}/
+
+# fix m4v files
+aws s3 cp \
+      --exclude "*" \
+      --include "*.m4v" \
+      --content-type="video/mp4"  \
+      --cache-control="public, max-age=31536000, immutable" \
+      --metadata-directive="REPLACE" \
+      --recursive \
+       s3://${BUCKET_NAME}/ \
+       s3://${BUCKET_NAME}/
+
+# fix woff files
+aws s3 cp \
+      --exclude "*" \
+      --include "*.woff" \
+      --content-type="font/woff"  \
+      --cache-control="public, max-age=31536000, immutable" \
+      --metadata-directive="REPLACE" \
+      --recursive \
+       s3://${BUCKET_NAME}/ \
+       s3://${BUCKET_NAME}/
+
+# fix woff2 files
+aws s3 cp \
+      --exclude "*" \
+      --include "*.woff2" \
+      --content-type="font/woff2"  \
+      --cache-control="public, max-age=31536000, immutable" \
+      --metadata-directive="REPLACE" \
+      --recursive \
+       s3://${BUCKET_NAME}/ \
+       s3://${BUCKET_NAME}/
+
+
+# remove all magnoliafabrics-$version.min.js files from demo folder
+rm ./browser-lib/demo/magnoliafabrics-*.min.js
+
+# Because PWAs caching will prevent seeing the latest changes, we need to invalidate the cache
+aws cloudfront create-invalidation --distribution-id ${CLOUDFRONT_DISTRIBUTION} --paths '/*'
 
 # create commit and tag(and update latest tag)
 git add .
@@ -73,17 +280,3 @@ else
     echo "Deployment failure"
     exit
 fi
-
-# publish to github registry
-cd npm-lib
-npm publish
-cd ..
-
-if [ $? -eq 0 ]; then
-    echo "NPM lib published successfully"
-else
-    echo "NPM lib publish failed"
-    exit
-fi
-
-# publish SPA-Example?
