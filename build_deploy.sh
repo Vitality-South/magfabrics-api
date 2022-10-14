0cbd4c0870655ff4cd50ef2f5123bfc28dc1345d
zachacious <back4moore@gmail.com>
2022-10-14 14:58:22 -0500

    Updated builds, create tags, bump version to 1.1.25

--- build_deploy.sh
+++ build_deploy.sh
@@ -63,13 +63,10 @@ else
     exit
 fi
 
-# get checksum and add to script tag and README
-checksum=$(cat browser-lib/dist/magnoliafabrics.min.js | openssl dgst -sha384 -binary | openssl base64 -A)
-sriRegex="src=\"(.*)magnoliafabrics-(.*).min.js\"[\n\r\s]+integrity=\"(.*)\""
-checksumNewIndex="src=\"magnoliafabrics-$version.min.js\"\nintegrity=\"sha384-$checksum\""
-checksumNewReadme="src=\"https://magnolia.vitalitysouth.com/magnoliafabrics-$version.min.js\"\nintegrity=\"sha384-$checksum\""
-sed -i -E 's/$sriRegex/$checksumNewReadme/' ./README.md
-sed -i -E 's/$sriRegex/$checksumNewIndex/' ./browser-lib/demo/index.html
+# get checksum and update in demo html script tag and README
+cd updateSRI
+node index.js
+cd ..
 
 # create a copy of browser lib build and add version number - save in versions folder
 cp ./browser-lib/dist/magnoliafabrics.min.js ./browser-lib/versions/magnoliafabrics-$version.min.js
---
+++ updateSRI/index.js
@@ -0,0 +1,33 @@
+"use strict";
+
+const fs = require("fs");
+const getSRI = require("get-sri");
+
+const version = fs.readFileSync("../VERSION").toString();
+const readmeFile = "../README.md";
+const demoIndexFile = "../browser-lib/demo/index.html";
+const browserDistFile = "../browser-lib/dist/magnoliafabrics.min.js";
+
+const browserDistFileData = fs.readFileSync(browserDistFile).toString("utf-8");
+const checksum = getSRI(browserDistFileData, getSRI.SHA384).toString();
+
+const sriRegex =
+  /src=\"(.*)magnoliafabrics(-?)(.*).min.js\"[\n\r\s]+integrity=\"(.*)\"/;
+const checksumNewIndex = `src="magnoliafabrics-${version}.min.js"\nintegrity="sha384-${checksum}"`;
+const checksumNewReadme = `src="https://magnolia.vitalitysouth.com/magnoliafabrics-${version}.min.js"\nintegrity="sha384-${checksum}"`;
+
+const currentDemoIndexFile = fs.readFileSync(demoIndexFile).toString();
+const currentReadmeFile = fs.readFileSync(readmeFile).toString();
+
+const newDemoIndexFile = currentDemoIndexFile.replace(
+  sriRegex,
+  checksumNewIndex
+);
+const newReadmeFile = currentReadmeFile.replace(sriRegex, checksumNewReadme);
+
+fs.writeFileSync(demoIndexFile, newDemoIndexFile);
+fs.writeFileSync(readmeFile, newReadmeFile);
+
+console.log(
+  "Successfully updated SRI hashes in browser-lib/demo/index.html and README.md"
+);
