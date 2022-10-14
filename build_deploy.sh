87726776d7901786721fa99e8f69ead1a83bf6e2
zachacious <back4moore@gmail.com>
2022-10-14 10:16:35 -0500

    Updated builds, create tags, bump version to 1.1.16

--- build_deploy.sh
+++ build_deploy.sh
@@ -66,10 +66,10 @@ fi
 # get checksum and add to script tag and README
 checksum=$(cat browser-lib/dist/magnoliafabrics.min.js | openssl dgst -sha384 -binary | openssl base64 -A
 )
-sriRegex="src=\"magnoliafabrics-(.*).min.js\"[\n\r\s]+integrity=\"(.*)\""
+sriRegex="src=\"magnoliafabrics-(.*).min.js\"[\n\r\s]*integrity=\"(.*)\""
 checksumNew="src=\"magnoliafabrics-$version.min.js\"\nintegrity=\"sha384-$checksum\""
-sed -i.bak -e "s/$sriRegex%$checksumNew%" ./README.md
-sed -i.bak -e "s/$sriRegex%$checksumNew%" ./browser-lib/demo/index.html
+sed -i.bak -e "s%$sriRegex%$checksumNew%" ./README.md
+sed -i.bak -e "s%$sriRegex%$checksumNew%" ./browser-lib/demo/index.html
 
 # create a copy of browser lib build and add version number - save in versions folder
 cp ./browser-lib/dist/magnoliafabrics.min.js ./browser-lib/versions/magnoliafabrics-$version.min.js
