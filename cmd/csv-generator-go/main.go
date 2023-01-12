dc367e776aa546260bb650c925eb607b21ff2563
Chris Elkins <celkins@vitalitysouth.com>
2023-01-12 16:50:02 -0600

    updated FAQ doc and started Shopify support for the CSV generator

--- cmd/csv-generator-go/main.go
+++ cmd/csv-generator-go/main.go
@@ -14,6 +14,7 @@ import (
 	"io"
 	"log"
 	"os"
+	"strings"
 	"unicode/utf8"
 
 	"github.com/Vitality-South/magfabrics-api/pkg/client"
@@ -32,6 +33,60 @@ const (
 	fabricDiscontinuedStatus = "X"
 )
 
+type shopifyProduct struct {
+	Handle                      string `csv:"Handle"`
+	Title                       string `csv:"Title"`
+	BodyHTML                    string `csv:"Body (HTML)"`
+	Vendor                      string `csv:"Vendor"`
+	ProductCategory             string `csv:"Product Category"`
+	Type                        string `csv:"Type"`
+	Tags                        string `csv:"Tags"`
+	Published                   string `csv:"Published"`
+	Option1Name                 string `csv:"Option1 Name"`
+	Option1Value                string `csv:"Option1 Value"`
+	Option2Name                 string `csv:"Option2 Name"`
+	Option2Value                string `csv:"Option2 Value"`
+	Option3Name                 string `csv:"Option3 Name"`
+	Option3Value                string `csv:"Option3 Value"`
+	VariantSKU                  string `csv:"Variant SKU"`
+	VariantGrams                string `csv:"Variant Grams"`
+	VariantInventoryTracker     string `csv:"Variant Inventory Tracker"`
+	VariantInventoryQty         string `csv:"Variant Inventory Qty"`
+	VariantInventoryPolicy      string `csv:"Variant Inventory Policy"`
+	VariantFulfillmentService   string `csv:"Variant Fulfillment Service"`
+	VariantPrice                string `csv:"Variant Price"`
+	VariantCompareAtPrice       string `csv:"Variant Compare At Price"`
+	VariantRequiresShipping     string `csv:"Variant Requires Shipping"`
+	VariantTaxable              string `csv:"Variant Taxable"`
+	VariantBarcode              string `csv:"Variant Barcode"`
+	ImageSrc                    string `csv:"Image Src"`
+	ImagePosition               string `csv:"Image Position"`
+	ImageAltText                string `csv:"Image Alt Text"`
+	GiftCard                    string `csv:"Gift Card"`
+	SEOTitle                    string `csv:"SEO Title"`
+	SEODescription              string `csv:"SEO Description"`
+	GoogleProductCategory       string `csv:"Google Shopping / Google Product Category"`
+	Gender                      string `csv:"Google Shopping / Gender"`
+	AgeGroup                    string `csv:"Google Shopping / Age Group"`
+	MPN                         string `csv:"Google Shopping / MPN"`
+	AdWordsGrouping             string `csv:"Google Shopping / AdWords Grouping"`
+	AdWordsLabels               string `csv:"Google Shopping / AdWords Labels"`
+	Condition                   string `csv:"Google Shopping / Condition"`
+	CustomProduct               string `csv:"Google Shopping / Custom Product"`
+	CustomLabel0                string `csv:"Google Shopping / Custom Label 0"`
+	CustomLabel1                string `csv:"Google Shopping / Custom Label 1"`
+	CustomLabel2                string `csv:"Google Shopping / Custom Label 2"`
+	CustomLabel3                string `csv:"Google Shopping / Custom Label 3"`
+	CustomLabel4                string `csv:"Google Shopping / Custom Label 4"`
+	VariantImage                string `csv:"Variant Image"`
+	VariantWeightUnit           string `csv:"Variant Weight Unit"`
+	VariantTaxCode              string `csv:"Variant Tax Code"`
+	CostPerItem                 string `csv:"Cost per item"`
+	PriceInternational          string `csv:"Price / International"`
+	CompareAtPriceInternational string `csv:"Compare At Price / International"`
+	Status                      string `csv:"Status"`
+}
+
 // filter returns a slice that contains only the elements of the provided slice
 // "ss" that return true with the given test function
 func filter[T any](ss []T, test func(T) bool) []T {
@@ -49,6 +104,22 @@ func filter[T any](ss []T, test func(T) bool) []T {
 // isNotDiscontinued returns true if the fabric is not discontinued
 func isNotDiscontinued(f *fabric.Fabric) bool {
 	return f.Status != fabricDiscontinuedStatus
+}
+
+func nospace(s string) string {
+	return strings.ReplaceAll(s, " ", "-")
+}
+
+func shopifyProducts(fabrics []*fabric.Fabric) []shopifyProduct {
+	products := []shopifyProduct{}
+
+	for _, f := range fabrics {
+		products = append(products, shopifyProduct{
+			Handle: nospace(f.ProductCode),
+		})
+	}
+
+	return products
 }
 
 func main() {
@@ -71,6 +142,8 @@ func main() {
 	apiKey := flag.String("apikey", "", "API `key` for MagFabrics API (required)")
 
 	ignoreDiscontinued := flag.Bool("ignorediscontinued", false, "ignore discontinued fabrics (default is false)")
+
+	shopify := flag.Bool("shopify", false, "Shopify csv output format (default is false)")
 
 	printVersion := flag.Bool("version", false, "print version number and exit")
 
@@ -122,6 +195,14 @@ func main() {
 		return filtered
 	}()
 
+	data := func() interface{} {
+		if *shopify {
+			return shopifyProducts(fabrics)
+		}
+
+		return fabrics
+	}
+
 	// convert fabrics data structure to CSV string
 	gocsv.SetCSVWriter(func(out io.Writer) *gocsv.SafeCSVWriter {
 		writer := csv.NewWriter(out)
@@ -129,12 +210,12 @@ func main() {
 		return gocsv.NewSafeCSVWriter(writer)
 	})
 
-	s, serr := gocsv.MarshalString(&fabrics)
+	csvOutput, serr := gocsv.MarshalString(&data)
 
 	if serr != nil {
 		log.Fatalf("error converting to csv: %v", serr)
 	}
 
 	// print CSV string to standard output
-	fmt.Print(s)
+	fmt.Print(csvOutput)
 }
--- docs/README.md
+++ docs/README.md
@@ -9,3 +9,9 @@ We recommend ignoring fabrics with status X and treating them as if they have al
 ### How frequently is fabric data updated?  
 
 The MagFabrics database (data in the API) is updated multiple times per day. The API always provides the latest data. You are free to query the API as often as you prefer.
+
+### What price should I display online? What happened to MAP price?  
+
+The "Display Price" and "Price" fields represent the only price that should be displayed online. No other pricing should be displayed online. The "Display Price" field is a string data type, contains the dollar sign, and is included for convenience of providing a display ready value. The "Price" field can also be used; it is an integer data type in pennies instead of dollars but otherwise represents the same value as the "Display Price". This field was added for the convenience of working with a numeric data type.  
+
+MagFabrics has discontinued the previously discounted price level referred to as "MAP" and replaced it with the regular Retail price. In short, "Retail" IS the new "MAP", which is why that is the only pricing included in our available data, and it must not be deviated from. No product names, color names, specs or images should be altered, nor pricing recalculated in any way.
