dc72a9cc4a542048cdae653e41b5531078c12e07
Chris Elkins <celkins@vitalitysouth.com>
2023-01-19 13:02:24 -0600

    updated build script for csv generator

--- cmd/csv-generator-go/build.sh
+++ cmd/csv-generator-go/build.sh
@@ -1,21 +1,29 @@
 VERSION=3
+CLOUDFRONT_DISTRIBUTION=ET9D78QSKOJQ6
+STORAGE=s3://images.magnoliaco.com/apps
+APP=magfabrics-csv
+CACHECONTROL='public, max-age=604800'
+CONTENTTYPE=application/octet-stream
 
 # cleanup binaries
 rm -f *.exe csv-generator-go
 
 # Windows
-GOOS=windows GOARCH=amd64 go build -ldflags="-s -w" -o magfabrics-csv-win-amd64-v${VERSION}.exe
-GOOS=windows GOARCH=386 go build -ldflags="-s -w" -o magfabrics-csv-win-386-v${VERSION}.exe
+GOOS=windows GOARCH=amd64 go build -ldflags="-s -w" -o ${APP}-win-amd64-v${VERSION}.exe
+GOOS=windows GOARCH=386 go build -ldflags="-s -w" -o ${APP}-win-386-v${VERSION}.exe
 
 # macOS
-GOOS=darwin GOARCH=amd64 go build -ldflags="-s -w" -o magfabrics-csv-macos-amd64-v${VERSION}
-GOOS=darwin GOARCH=arm64 go build -ldflags="-s -w" -o magfabrics-csv-macos-arm64-v${VERSION}
+GOOS=darwin GOARCH=amd64 go build -ldflags="-s -w" -o ${APP}-macos-amd64-v${VERSION}
+GOOS=darwin GOARCH=arm64 go build -ldflags="-s -w" -o ${APP}-macos-arm64-v${VERSION}
 
 # native platform
 #go build -ldflags="-s -w" -o csv-generator-go
 
 # copy binaries to S3 - accessible via https://images.magfabrics.com
-aws s3 cp --cache-control "public, max-age=604800" --content-type "application/octet-stream" magfabrics-csv-win-amd64-v${VERSION}.exe s3://images.magnoliaco.com/apps/magfabrics-csv-win-amd64-v${VERSION}.exe
-aws s3 cp --cache-control "public, max-age=604800" --content-type "application/octet-stream" magfabrics-csv-win-386-v${VERSION}.exe s3://images.magnoliaco.com/apps/magfabrics-csv-win-386-v${VERSION}.exe
-aws s3 cp --cache-control "public, max-age=604800" --content-type "application/octet-stream" magfabrics-csv-macos-amd64-v${VERSION} s3://images.magnoliaco.com/apps/magfabrics-csv-macos-amd64-v${VERSION}
-aws s3 cp --cache-control "public, max-age=604800" --content-type "application/octet-stream" magfabrics-csv-macos-arm64-v${VERSION} s3://images.magnoliaco.com/apps/magfabrics-csv-macos-arm64-v${VERSION}
+aws s3 cp --cache-control "${CACHECONTROL}" --content-type ${CONTENTTYPE} --metadata-directive="REPLACE" ${APP}-win-amd64-v${VERSION}.exe ${STORAGE}/${APP}-win-amd64-v${VERSION}.exe
+aws s3 cp --cache-control "${CACHECONTROL}" --content-type ${CONTENTTYPE} --metadata-directive="REPLACE" ${APP}-win-386-v${VERSION}.exe ${STORAGE}/${APP}-win-386-v${VERSION}.exe
+aws s3 cp --cache-control "${CACHECONTROL}" --content-type ${CONTENTTYPE} --metadata-directive="REPLACE" ${APP}-macos-amd64-v${VERSION} ${STORAGE}/${APP}-macos-amd64-v${VERSION}
+aws s3 cp --cache-control "${CACHECONTROL}" --content-type ${CONTENTTYPE} --metadata-directive="REPLACE" ${APP}-macos-arm64-v${VERSION} ${STORAGE}/${APP}-macos-arm64-v${VERSION}
+
+# invalidate cloudfront cache
+aws cloudfront create-invalidation --distribution-id ${CLOUDFRONT_DISTRIBUTION} --paths '/*'
