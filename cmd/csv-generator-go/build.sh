f5aaa9d215101be915d3a5cbf65c3368542f55a4
Chris Elkins <celkins@vitalitysouth.com>
2023-01-19 12:42:39 -0600

    fix csv generator building for windows

--- cmd/csv-generator-go/build.sh
+++ cmd/csv-generator-go/build.sh
@@ -15,7 +15,7 @@ GOOS=darwin GOARCH=arm64 go build -ldflags="-s -w" -o magfabrics-csv-macos-arm64
 #go build -ldflags="-s -w" -o csv-generator-go
 
 # copy binaries to S3 - accessible via https://images.magfabrics.com
-aws s3 cp magfabrics-csv-amd64-v${VERSION}.exe s3://images.magnoliaco.com/apps/magfabrics-csv-amd64-v${VERSION}.exe
-aws s3 cp magfabrics-csv-386-v${VERSION}.exe s3://images.magnoliaco.com/apps/magfabrics-csv-386-v${VERSION}.exe
-aws s3 cp magfabrics-csv-macos-amd64-v${VERSION} s3://images.magnoliaco.com/apps/magfabrics-csv-macos-amd64-v${VERSION}
-aws s3 cp magfabrics-csv-macos-arm64-v${VERSION} s3://images.magnoliaco.com/apps/magfabrics-csv-macos-arm64-v${VERSION}
+aws s3 cp --cache-control "public, max-age=604800" --content-type "application/octet-stream" magfabrics-csv-win-amd64-v${VERSION}.exe s3://images.magnoliaco.com/apps/magfabrics-csv-win-amd64-v${VERSION}.exe
+aws s3 cp --cache-control "public, max-age=604800" --content-type "application/octet-stream" magfabrics-csv-win-386-v${VERSION}.exe s3://images.magnoliaco.com/apps/magfabrics-csv-win-386-v${VERSION}.exe
+aws s3 cp --cache-control "public, max-age=604800" --content-type "application/octet-stream" magfabrics-csv-macos-amd64-v${VERSION} s3://images.magnoliaco.com/apps/magfabrics-csv-macos-amd64-v${VERSION}
+aws s3 cp --cache-control "public, max-age=604800" --content-type "application/octet-stream" magfabrics-csv-macos-arm64-v${VERSION} s3://images.magnoliaco.com/apps/magfabrics-csv-macos-arm64-v${VERSION}
