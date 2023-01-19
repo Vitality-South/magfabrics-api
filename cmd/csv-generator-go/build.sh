7f4cca781775d1f5719e6f69f57611c0d3585392
Chris Elkins <celkins@vitalitysouth.com>
2023-01-19 11:00:34 -0600

    auth handling during build

--- cmd/csv-generator-go/build.sh
+++ cmd/csv-generator-go/build.sh
@@ -3,3 +3,4 @@ rm -f *.exe csv-generator-go
 GOOS=windows GOARCH=amd64 go build -ldflags="-s -w" -o magfabrics-csv-amd64-v${VERSION}.exe
 GOOS=windows GOARCH=386 go build -ldflags="-s -w" -o magfabrics-csv-386-v${VERSION}.exe
 go build -ldflags="-s -w" -o csv-generator-go
+#aws s3 cp  s3://images.magnoliaco.com/apps/
